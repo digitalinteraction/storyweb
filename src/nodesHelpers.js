73323aeb79a4cb588bdd7c3181e4b85f8f6c1d27
Tom Feltwell <tfeltwell@gmail.com>
2021-11-04 15:25:33 +0000

    style(instructions): Makes instruction text 4rem size (much bigger) to attract attention

--- src/nodesHelpers.js
+++ src/nodesHelpers.js
@@ -77,10 +77,10 @@ export function defaultTemplate() {
       <h3 class="card-title">Story:Web</h3>
       <p class="card-text">This Story:Web is made up of objects from the Great North Museum: Hancock, images and sounds shared online, and public contributions from social media and elsewhere. It will grow during the exhibition.</p>
       <p class="card-text">Help us work out where the stories go next on social media using #GrowStoryWeb, and your words, images and sounds could become part of the exhibition.</p>
-      <p class="card-text">
+      <p class="card-text instruct-text">
         <strong>Tap a picture to start exploring.</strong>
       </p>
-      <p class="card-text">
+      <p class="card-text instruct-text">
         <strong>Plug in your headphones to hear the sounds in 3D.</strong>
       </p>
     </div>
